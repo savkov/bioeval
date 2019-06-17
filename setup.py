2d1511fd6732fbe1b7450bccc20bfad2350c03f3
Sasho Savkov <me@sasho.io>
2019-06-18 00:07:37 +0100

    Inproved setup files

--- setup.py
+++ setup.py
@@ -1,14 +1,47 @@
 #!/usr/bin/env python
-from setuptools import find_packages
+import os
+
 from distutils.core import setup
+from setuptools import find_packages
 
 __author__ = 'Aleksandar Savkov'
 
-setup(name='bioeval',
-      version='1.1',
-      description='BIO and BEISO evaluation library',
-      author='Aleksandar Savkov',
-      author_email='aleksandar@savkov.eu',
-      url='https://www.github.com/asavkov/bioeval/',
-      packages=find_packages()
-     )
+
+# allow setup.py to be run from any path
+this_dir = os.path.abspath(os.path.dirname(__file__))
+os.chdir(os.path.normpath(this_dir))
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()[1:]
+
+with open('README.md') as fh:
+    description = fh.read()
+
+setup(
+    name='bioeval',
+    version='1.1.1',
+    description='BIO and BEISO evaluation library',
+    author='Sasho Savkov',
+    author_email='me@sasho.io',
+    url='https://github.com/savkov/bioeval',
+    packages=find_packages(exclude=('tests', 'prl', 'res')),
+    zip_safe=True,
+    include_package_data=True,
+    license='MIT',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Operating System :: Unix',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering'
+    ]
+)
